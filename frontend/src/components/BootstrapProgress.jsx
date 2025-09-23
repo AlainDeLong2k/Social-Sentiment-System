396f7f0d200867d0a97799e769f67570b63b77fd
Alain De Long <leholong007@gmail.com>
2025-09-23 15:17:19 +0700

    feat(pages): add analysis page

---
+++ frontend/src/components/BootstrapProgress.jsx
@@ -0,0 +1,49 @@
+export const BootstrapProgress = ({ positive, negative, neutral }) => {
+  return (
+    <div className="pro">
+      <div
+        className="progress"
+        role="progressbar"
+        aria-label="Success example 40px high"
+        aria-valuenow="25"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        style={{ height: "40px" }}
+      >
+        <div className="progress-bar bg-success" style={{ width: positive }}>
+          POSITIVE: {positive}
+        </div>
+      </div>
+      <div
+        className="progress"
+        role="progressbar"
+        aria-label="Warning example 50px high"
+        aria-valuenow="75"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        style={{ height: "40px" }}
+      >
+        <div
+          className="progress-bar bg-warning text-dark"
+          style={{ width: neutral }}
+        >
+          Neutral: {neutral}
+        </div>
+      </div>
+      <div
+        className="progress"
+        role="progressbar"
+        aria-label="Warning example 50px high"
+        aria-valuenow="75"
+        aria-valuemin="0"
+        aria-valuemax="100"
+        style={{ height: "40px" }}
+      >
+        <div className="progress-bar bg-danger" style={{ width: negative }}>
+          Negative: {negative}
+        </div>
+      </div>
+      <br />
+    </div>
+  );
+};
