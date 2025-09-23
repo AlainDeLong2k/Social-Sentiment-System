478ce1c0054fdcda832309387a25a14ed7044c93
Alain De Long <leholong007@gmail.com>
2025-09-23 09:57:24 +0700

    feat: add components, pages and routes folder

---
+++ frontend/src/components/BootstrapCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+export const BootstrapCard = ({ img, name, desc }) => {
+  return (
+    <div>
+      <div class="card" style={{ width: "18rem", height: "20rem" }}>
+        <img src={img} class="card-img-top" alt=" " />
+        <div class="card-body">
+          <h5 class="card-title">{name}</h5>
+          <p class="card-text"> {desc}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
